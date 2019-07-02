b419ebe9f662d9708be6de8f10f160700a9618c4
Aaron <aaron.j.moreno@saic.com>
2019-07-02 10:45:46 -0600

    Added sign up button functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
   get 'static_pages/about'
+  get 'signup' => 'users#new'
   root 'static_pages#home'
 end

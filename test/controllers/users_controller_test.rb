da3519972a9f8833d7f232027fc42bffd8bf2e7f
Aaron <aaron.j.moreno@saic.com>
2019-07-02 13:22:34 -0600

    added tests and fixed gem vuln

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,4 +24,31 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @user.valid?
   end
 
+  test "name should be less than 50 characters" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should be less than 50 characters" do
+    @user.email = "a" * 100
+    assert_not @user.valid?
+  end
+
+  test "should be a valid email" do
+    valid_addresses = %w[user@example.com USER@FOOBAR.COM A_EMAIL-USER@FOOBAR.NEW.COM]
+    
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?
+    end
+  end
+
+  test "should be invalid emails" do
+    invalid_addresses = %w[user@example,com foo_at_bar.com user_name@example. foobar@baz_foo.com foo@baz+bar.com]
+
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "Address valid #{invalid_address.inspect} should be invalid"
+    end
+  end
 end
